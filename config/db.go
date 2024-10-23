8a84ef9a6fff44979516186603927bf7319953c7
fajaaro <fajarhamdani70@gmail.com>
2024-10-23 16:40:55 +0700

    implement .env

--- config/db.go
+++ config/db.go
@@ -3,7 +3,9 @@ package config
 import (
 	"learn/models"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,8 +13,18 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-    dsn := "root:@tcp(127.0.0.1:3306)/golearn?charset=utf8mb4&parseTime=True&loc=Local"
-    var err error
+    err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+    
+    dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
     DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Failed to connect to database:", err)
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"learn/config"
 	"learn/routes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
     config.ConnectDB()
 
     r := routes.SetupRouter()
 
-    r.Run(":8080")
+    r.Run(":" + os.Getenv("PORT"))
 }
